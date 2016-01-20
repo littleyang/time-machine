4cf9fbe7d77bf7ce43d62bb1683c67d167359e6d
littleyang <littleyangqh@gmail.com>
2016-01-20 11:57:17 +0800

    init data

---
+++ src/test/java/com/vanke/test/base/BaseTest.java
@@ -0,0 +1,11 @@
+package com.vanke.test.base;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class) 
+@ContextConfiguration(locations={"classpath:application-contex-test.xml"}) 
+public class BaseTest {
+
+}
