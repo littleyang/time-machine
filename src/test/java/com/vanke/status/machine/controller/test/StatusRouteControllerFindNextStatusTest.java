db922da2550bd6b19417b7c8e7f5a235991ac001
littleyang <littleyangqh@gmail.com>
2016-01-20 15:03:48 +0800

    create controller test

--- src/test/java/com/vanke/status/machine/controller/test/StatusRouteControllerFindNextStatusTest.java
+++ src/test/java/com/vanke/status/machine/controller/test/StatusRouteControllerFindNextStatusTest.java
@@ -11,7 +11,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.mockito.Mockito.*;
 
 import com.vanke.status.machine.controller.StatusRouteController;
