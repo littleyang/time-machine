b4f857aaf09c4836ae4a9d55b1c8413ca514162c
littleyang <littleyangqh@gmail.com>
2016-01-21 09:51:04 +0800

    modify sql

--- sql/2016-01-20-create-status-machine.sql
+++ sql/2016-01-20-create-status-machine.sql
@@ -1,10 +1,9 @@
 create table `status_route` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`bussiness_type` varchar (32) NOT NULL,
+	`current_event` varchar (32) NOT NULL,
 	`current_status` int(6) NOT NULL,
 	`next_status` int(6) NOT NULL,
-	`event` varchar (32) NOT NULL,
-	`event_msg` varchar (64) DEFAULT NULL,
 	`created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`)
