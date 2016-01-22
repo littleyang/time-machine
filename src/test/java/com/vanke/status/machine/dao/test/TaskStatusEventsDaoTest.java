9bc9a0441e9b94bb098193d7440a85c20d65a4e3
littleyang <littleyangqh@gmail.com>
2016-01-22 09:30:16 +0800

    run test data

--- src/test/java/com/vanke/status/machine/dao/test/TaskStatusEventsDaoTest.java
+++ src/test/java/com/vanke/status/machine/dao/test/TaskStatusEventsDaoTest.java
@@ -5,11 +5,16 @@ import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.vanke.status.machine.dao.StatusEventsDao;
 import com.vanke.test.base.BaseTest;
 
 public class TaskStatusEventsDaoTest extends BaseTest{
+	
+	@Mock
+	private JdbcTemplate jdbcTemplate;
 	
 	@InjectMocks
 	private StatusEventsDao eventsDao;
@@ -18,10 +23,6 @@ public class TaskStatusEventsDaoTest extends BaseTest{
 	public void testGetAllStatusEventsShouldBeZero(){
 		assertThat(eventsDao.getAllStatusEventsCount(), is(0));
 	}
-	
-	@Test 
-	public void exportData(){
-		
-	}
+
 
 }
