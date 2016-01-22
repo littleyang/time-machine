27d6da063f2f5d0b27fb86e70db24a2b95148264
littleyang <littleyangqh@gmail.com>
2016-01-22 17:10:39 +0800

    orm

--- src/test/java/com/vanke/status/machine/dao/test/TaskEventsDaoTest.java
+++ src/test/java/com/vanke/status/machine/dao/test/TaskEventsDaoTest.java
@@ -4,9 +4,9 @@ import org.junit.Test;
 import org.unitils.spring.annotation.SpringBeanByType;
 
 import com.vanke.status.machine.dao.TaskEventsDao;
-import com.vanke.test.base.BaseTestUnit;
+import com.vanke.test.base.BaseDaoTestBeans;
 
-public class TaskEventsDaoTest extends BaseTestUnit{
+public class TaskEventsDaoTest extends BaseDaoTestBeans{
 	
 	@SpringBeanByType  
 	private TaskEventsDao eventsDao;
