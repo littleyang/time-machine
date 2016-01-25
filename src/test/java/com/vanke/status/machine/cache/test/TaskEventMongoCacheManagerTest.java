fb3282618e33c5505230e4f3cd165307ef21fab9
littleyang <littleyangqh@gmail.com>
2016-01-25 15:29:14 +0800

    add mongo crud

--- src/test/java/com/vanke/status/machine/cache/test/TaskEventMongoCacheManagerTest.java
+++ src/test/java/com/vanke/status/machine/cache/test/TaskEventMongoCacheManagerTest.java
@@ -6,9 +6,7 @@ import static org.junit.Assert.assertThat;
 import java.util.Date;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -25,19 +23,6 @@ public class TaskEventMongoCacheManagerTest extends BaseTestUnit{
 	
 	private String eventKey;
 	
-//	@BeforeClass
-//	public static void setUp(){
-//		System.out.println("====setup all test data=====");
-//		event = new TaskEvents();
-//		event.setId(10000);
-//		event.setCode("E100000");
-//		event.setName("test-task-event");
-//		event.setType(1);
-//		event.setMsg("test-task-event-msg");
-//		event.setCreated(new Date());
-//		event.setUpdated(new Date());
-//		eventKey = "task_events";
-//	}
 	
 	@Before
 	public void setEachTest(){
@@ -59,26 +44,19 @@ public class TaskEventMongoCacheManagerTest extends BaseTestUnit{
 		taskEventMongoCacheManager.getMongoTemplate().dropCollection(eventKey);
 	}
 	
-//	@AfterClass
-//	public static void clean(){
-//		System.out.println("====clean all test data=====");
-//		taskEventMongoCacheManager.getMongoTemplate().dropCollection(eventKey);
-//	}
-	
+
 	@Test
-	public void testCreateTaskEventsToMongo(){
-				
-		taskEventMongoCacheManager.insertValue(event, eventKey);
-	
+	public void testCreateTaskEventsToMongo(){		
+		taskEventMongoCacheManager.create(event);
 	}
 	
 	
 	@Test
 	public void testGetTaskEventsByIdFromMongo(){
 		
-		taskEventMongoCacheManager.insertValue(event, eventKey);
+		taskEventMongoCacheManager.create(event);
 		
-		TaskEvents eventTemp = (TaskEvents) taskEventMongoCacheManager.findOneById(event.getId(), eventKey);
+		TaskEvents eventTemp = (TaskEvents) taskEventMongoCacheManager.findOneById(event.getId());
 		
 		assertThat("task event id should be equal",event.getId(), is(eventTemp.getId()));
 		
@@ -87,11 +65,29 @@ public class TaskEventMongoCacheManagerTest extends BaseTestUnit{
 	@Test
 	public void testGetTaskEventsByCodeFromMongo(){
 		
-		taskEventMongoCacheManager.insertValue(event, eventKey);
+		taskEventMongoCacheManager.create(event);
 		
-		TaskEvents eventTemp = (TaskEvents) taskEventMongoCacheManager.findOneByCode(event.getCode(), eventKey);
+		TaskEvents eventTemp = (TaskEvents) taskEventMongoCacheManager.findOneByCode(event.getCode());
 		
 		assertThat("task event id should be equal",eventTemp.getCode(), is(eventTemp.getCode()));
+		
+	}
+	
+	@Test
+	public void testUpdateTaskEvents(){
+		
+		taskEventMongoCacheManager.create(event);
+		
+		TaskEvents eventTemp = (TaskEvents) taskEventMongoCacheManager.findOneByCode(event.getCode());
+		
+		eventTemp.setMsg("this is update msssssss");
+		
+		taskEventMongoCacheManager.updateOne(eventTemp);
+		
+		TaskEvents eventTempAfterUpdate = (TaskEvents) taskEventMongoCacheManager.findOneByCode(event.getCode());
+		
+		assertThat("task event id should be equal",eventTemp.getCode(), is(eventTempAfterUpdate.getCode()));
+		assertThat("task event id should be equal",eventTemp.getMsg(), is(eventTempAfterUpdate.getMsg()));
 		
 	}
 
