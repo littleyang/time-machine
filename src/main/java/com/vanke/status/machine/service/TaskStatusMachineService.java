554cded4462203743bee46c68607c0c8e7bb2979
littleyang <littleyangqh@gmail.com>
2016-01-28 16:20:44 +0800

    add test

---
+++ src/main/java/com/vanke/status/machine/service/TaskStatusMachineService.java
@@ -0,0 +1,10 @@
+package com.vanke.status.machine.service;
+
+import org.springframework.stereotype.Service;
+
+@Service("taskStatusMachineService")
+public class TaskStatusMachineService {
+	
+	
+
+}
