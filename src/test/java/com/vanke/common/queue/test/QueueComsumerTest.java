40411e522f5fbe16bf1386be8ad7ab068422e52c
littleyang <littleyangqh@gmail.com>
2016-03-01 14:57:35 +0800

    add task message queue

--- src/test/java/com/vanke/common/queue/test/QueueComsumerTest.java
+++ src/test/java/com/vanke/common/queue/test/QueueComsumerTest.java
@@ -1,7 +1,11 @@
 package com.vanke.common.queue.test;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.vanke.common.model.base.BaseObject;
 import com.vanke.common.queue.comsumer.TaskComsumerMessageListener;
 import com.vanke.test.base.BaseTestUnit;
 
@@ -11,4 +15,25 @@ public class QueueComsumerTest extends BaseTestUnit{
 	@Autowired
 	private TaskComsumerMessageListener taskComsumerMessageListener;
 
+	private BaseObject testObject;
+	
+	@Before
+	public void setUp(){
+		System.out.println("====setup data=====");
+		testObject = new BaseObject();
+		testObject.setBaseType("CreateTask");
+	}
+	
+	
+	@After
+	public void cleanEachTest(){
+		System.out.println("====clean data=====");
+	}
+	
+	@Test
+	public void createTaskAndPushToTaskQueueTest(){
+		// 一般开发先注释掉
+		// messageQueueDispatcher.dispatchToTaskDestination(testObject);
+	}
+	
 }
