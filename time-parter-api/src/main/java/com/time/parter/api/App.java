5dec663898bac35d58bc1d4382dceaa48982c54f
littleyang <littleyangqh@gmail.com>
2016-04-02 16:56:42 +0800

    add soa arch

---
+++ time-parter-api/src/main/java/com/time/parter/api/App.java
@@ -0,0 +1,13 @@
+package com.time.parter.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
