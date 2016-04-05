9bfae8aa32ce5a3e6398c1684ce715c0abe54991
littleyang <littleyangqh@gmail.com>
2016-04-05 09:31:11 +0800

    change dubbo config files

--- time-common/src/main/java/com/time/registry/service/impl/RegistryServiceImpl.java
+++ time-common/src/main/java/com/time/registry/service/impl/RegistryServiceImpl.java
@@ -1,7 +1,10 @@
 package com.time.registry.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.time.registry.service.RegistryDemoService;
 
+@Service("registryDemoService")
 public class RegistryServiceImpl implements RegistryDemoService {
 
 	@Override
