7bb63fc38f8b80ad60e03a8f0f97814b49d15e66
littleyang <littleyangqh@gmail.com>
2016-04-06 10:40:44 +0800

    add web admin

--- time-web-admin/src/main/java/com/time/admin/controller/AdminIndexController.java
+++ time-web-admin/src/main/java/com/time/admin/controller/AdminIndexController.java
@@ -7,29 +7,25 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
+@RequestMapping("/index")
 public class AdminIndexController {
 
-	private static int counter = 0;
-	private static final String VIEW_INDEX = "hello";
 	private static final Log logger = LogFactory.getLog(AdminIndexController.class);
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String welcome(ModelMap model) {
-		model.addAttribute("message", "Welcome");
-		model.addAttribute("counter", ++counter);
-		logger.debug("[Welcome counter :{}" + counter);
-		return VIEW_INDEX;// 返回index.jsp
-	}
+	public ModelAndView login(){
+        return new ModelAndView("hello");
+    }
 
 	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
-	public String welcome(@PathVariable String name, ModelMap model) {
+	public ModelAndView welcome(@PathVariable String name, ModelMap model) {
 		model.addAttribute("message", "Welcome " + name);
-		model.addAttribute("counter", ++counter);
-		logger.debug("[Welcome counter :{}" + counter);
-		return VIEW_INDEX;// 返回index.jsp
+		logger.debug("[Welcome counter :{}" + name);
+		return new ModelAndView("hello");
 	}
 
 }
