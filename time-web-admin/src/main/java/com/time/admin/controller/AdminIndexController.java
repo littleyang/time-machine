ef113b232d4c0e871c7098587925fae067e6c2e9
littleyang <littleyangqh@gmail.com>
2016-04-06 21:05:28 +0800

    add web admin test

--- time-web-admin/src/main/java/com/time/admin/controller/AdminIndexController.java
+++ time-web-admin/src/main/java/com/time/admin/controller/AdminIndexController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 
-
 @Controller
 @RequestMapping("/index")
 public class AdminIndexController {
