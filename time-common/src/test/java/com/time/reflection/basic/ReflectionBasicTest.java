e83c784c497a63b84ed7c8f6a2c72a619a9454d6
littleyang <littleyangqh@gmail.com>
2016-05-20 17:25:53 +0800

    reflection method

---
+++ time-common/src/test/java/com/time/reflection/basic/ReflectionBasicTest.java
@@ -0,0 +1,77 @@
+package com.time.reflection.basic;
+
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReflectionBasicTest {
+	
+	@Test
+	public void testReflectionBasic() throws NoSuchMethodException, SecurityException, IllegalAccessException, 
+		IllegalArgumentException, InvocationTargetException, NoSuchFieldException{
+		
+		// according reflection get class name
+		Foo foo = new Foo();
+		
+		// class name
+		Assert.assertEquals("class name should be com.time.reflection.basic.Foo", 
+				"com.time.reflection.basic.Foo", foo.getClass().getName());
+		
+		// get reflection method 
+		Method method;
+		Assert.assertEquals("Foo class methods size ", 3 , foo.getClass().getDeclaredMethods().length);
+		
+		// get say hello method
+		method = foo.getClass().getMethod("sayHello", String.class);
+		method.invoke(foo,"zy");
+		
+		// get reflection proper
+		Field field;
+		Assert.assertEquals("Foo class methods size ", 1 , foo.getClass().getDeclaredFields().length);
+		
+		field = foo.getClass().getDeclaredField("name");
+		Assert.assertEquals("field name should be name ", "name", field.getName());
+		
+		// reflection create instance
+		Class<?> ins = null;
+		try {
+			ins = Class.forName("com.time.reflection.basic.Foo");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//创建一个实例
+		Foo fTemp = null;
+		try {
+			fTemp = (Foo) ins.newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// call say hello method 
+		Assert.assertNotNull("should not be null when call method", fTemp.sayHello("zy"));
+	}
+}
+
+class Foo{
+	
+	private String name ;
+	
+	public String sayHello(String name){
+		return name + " hello";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
