50a17e00725cc80fd5931c9faad007f06eea2396
littleyang <littleyangqh@gmail.com>
2016-05-26 14:35:19 +0800

    add position work

--- time-spark/src/main/java/com/time/spark/StaffCheckInStatic.java
+++ time-spark/src/main/java/com/time/spark/StaffCheckInStatic.java
@@ -55,5 +55,17 @@ public class StaffCheckInStatic {
 		});
 		System.out.println("员工结束工作数据" + staffEndWorkRdd.count());
 		
+		// 员工上报位置数据
+		JavaRDD<String> staffPositionRdd = staffCheckIns.filter(new Function<String,Boolean>(){
+			private static final long serialVersionUID = 1L;
+			public Boolean call(String x) throws Exception {
+			// 查看首页的功能的
+				if(x.contains("POST /api/lebang/staffs/me/work"))
+					return true;
+				return false;
+			}
+		});
+		System.out.println("员工上报位置数据" + staffPositionRdd.count());
+		
 	}
 }
