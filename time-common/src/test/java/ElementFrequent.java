c361278652a403e4c187115b5ccc814687900ea6
littleyang <littleyangqh@gmail.com>
2016-05-31 14:50:53 +0800

    add topK frequent

---
+++ time-common/src/test/java/ElementFrequent.java
@@ -0,0 +1,14 @@
+
+public class ElementFrequent implements Comparable{
+	
+	private int key;
+	
+	private int frequent;
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
