57a0b624159b2b3a09a8fde04b8452123547ba5e
littleyang <littleyangqh@gmail.com>
2016-06-07 09:15:43 +0800

    add hadoop

--- time-hadoop-data/src/test/java/com/time/mapreduce/test/data/DaliyUserJobControllerCount.java
+++ time-hadoop-data/src/test/java/com/time/mapreduce/test/data/DaliyUserJobControllerCount.java
@@ -2,7 +2,6 @@ package com.time.mapreduce.test.data;
 
 import java.io.IOException;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -11,7 +10,6 @@ import org.apache.hadoop.mapred.jobcontrol.JobControl;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.jobcontrol.ControlledJob;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
