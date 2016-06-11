d782a9d17c64b081ff1215cf44b51a8f4dfa14a3
littleyang <littleyangqh@gmail.com>
2016-06-11 15:54:46 +0800

    add second solution

--- time-common/src/test/java/RotateArray.java
+++ time-common/src/test/java/RotateArray.java
@@ -17,15 +17,42 @@ public class RotateArray {
 		    return;
 		}
 		
-		for(int i=0;i<k;i++){
-			int temp = arrays[size-k + i];
-			for(int j= size-k+i;j>i;j--){
-				arrays[j] = arrays[j-1];
-			}
-			arrays[i] = temp;
-		}
+		// first solution
+//		for(int i=0;i<k;i++){
+//			int temp = arrays[size-k + i];
+//			for(int j= size-k+i;j>i;j--){
+//				arrays[j] = arrays[j-1];
+//			}
+//			arrays[i] = temp;
+//		}
 		
+		
+//		// second solution
+//		if(k > arrays.length){
+//			k = k % arrays.length;
+//		}
+//		int a = arrays.length - k; 
+//	 
+//		reverse(arrays, 0, a-1);
+//		reverse(arrays, a, arrays.length-1);
+//		reverse(arrays, 0, arrays.length-1);
+		
+		
+		
+		
+	}
 	
+	public static void reverse(int[] arr, int left, int right){
+		if(arr == null || arr.length == 1) 
+			return;
+	 
+		while(left < right){
+			int temp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = temp;
+			left++;
+			right--;
+		}
 	}
 
 }
