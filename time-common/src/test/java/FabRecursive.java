c7f5d957147b36d1099726042c9736d9bd591a58
littleyang <littleyangqh@gmail.com>
2016-06-12 10:22:55 +0800

    add fab arrays

---
+++ time-common/src/test/java/FabRecursive.java
@@ -0,0 +1,23 @@
+
+public class FabRecursive {
+	
+	public static void main(String[] args){
+		
+		for(int i=0;i<=20;i++){
+			System.out.println(fab(i));
+		}
+		
+	}
+	
+	public static long fab(int n){
+		long result = 0L;
+		if(n==0)
+			result = 1;
+		if(n==1)
+			result = 1;
+		if(n>1)
+			result = fab(n-1)+ fab(n-2);
+		return result;
+	}
+
+}
