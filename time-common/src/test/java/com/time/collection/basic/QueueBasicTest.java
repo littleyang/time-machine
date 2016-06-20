6401e81bd8810f876285c2a11354aed13782d2c2
littleyang <littleyangqh@gmail.com>
2016-06-20 14:31:44 +0800

    multi thread blocking queue

--- time-common/src/test/java/com/time/collection/basic/QueueBasicTest.java
+++ time-common/src/test/java/com/time/collection/basic/QueueBasicTest.java
@@ -1,7 +1,9 @@
 package com.time.collection.basic;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.PriorityQueue;
+import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.junit.Test;
@@ -22,7 +24,7 @@ public class QueueBasicTest {
 		Iterator<String> it = queues.iterator();
 		
 		while(it.hasNext()){
-			String element = (String) it.next();
+			String element = it.next();
 			System.out.println(element);
 		}
 	}
@@ -45,11 +47,64 @@ public class QueueBasicTest {
 		Iterator<FIFOEntry<String>> it = queues.iterator();
 		
 		while(it.hasNext()){
-			FIFOEntry<String> element = (FIFOEntry<String>) it.next();
+			FIFOEntry<String> element = it.next();
+			System.out.println(element);
+		}
+	}
+	
+	
+	@Test
+	public void testPriorityBlockingQueueBasic(){
+		
+		PriorityBlockingQueue<FIFOEntry<String>> queues = new PriorityBlockingQueue<FIFOEntry<String>>(2);
+		
+		for(int i=0;i<Runtime.getRuntime().availableProcessors();i++){
+			
+			Thread runThread = new Thread(new Runnable(){
+
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					FIFOEntry<String> entityF = new FIFOEntry<String>("F" + Thread.currentThread());
+					
+					FIFOEntry<String> entityS = new FIFOEntry<String>("S" + Thread.currentThread());
+					
+					FIFOEntry<String> entityA = new FIFOEntry<String>("A" + Thread.currentThread());
+					
+					queues.add(entityF);
+					queues.add(entityS);
+					queues.add(entityA);
+					
+					printBlockingQueue(queues);
+				}
+			});
+			
+			runThread.start();
+			runThread.run();
+			
+		}
+	}
+	
+	public void printBlockingQueue(PriorityBlockingQueue<FIFOEntry<String>> queues){
+		
+		Iterator<FIFOEntry<String>> it = queues.iterator();
+		
+		while(it.hasNext()){
+			FIFOEntry<String> element = it.next();
 			System.out.println(element);
 		}
 	}
 
+}
+
+class EntryCompator implements Comparator<FIFOEntry<String>>{
+
+	@Override
+	public int compare(FIFOEntry<String> one, FIFOEntry<String> two) {
+		// TODO Auto-generated method stub
+		return one.compareTo(two);
+	}
+	
 }
 
 class FIFOEntry<E extends Comparable<? super E>> implements Comparable<FIFOEntry<E>> {
