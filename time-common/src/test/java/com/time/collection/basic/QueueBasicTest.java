71f0d3afd21ebbd8806d0fec32c7afa6eef6d8fe
littleyang <littleyangqh@gmail.com>
2016-06-20 13:19:44 +0800

    add priority queue

--- time-common/src/test/java/com/time/collection/basic/QueueBasicTest.java
+++ time-common/src/test/java/com/time/collection/basic/QueueBasicTest.java
@@ -1,5 +1,31 @@
 package com.time.collection.basic;
 
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+import org.junit.Test;
+
 public class QueueBasicTest {
+	
+	@Test
+	public void testPriorityQueueTests(){
+		
+		PriorityQueue<String> queues = new PriorityQueue<String>(2);
+		
+		queues.add("S");
+		
+		queues.add("F");
+		
+		queues.add("A");
+		
+		Iterator<String> it = queues.iterator();
+		
+		while(it.hasNext()){
+			String element = (String) it.next();
+			System.out.println(element);
+		}
+		
+		
+	}
 
 }
