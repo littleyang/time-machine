c68105c2097d947913208e7fec0038a5c1666f76
littleyang <littleyangqh@gmail.com>
2016-06-20 13:49:42 +0800

    add priority queue entity

--- time-common/src/test/java/com/time/collection/basic/QueueBasicTest.java
+++ time-common/src/test/java/com/time/collection/basic/QueueBasicTest.java
@@ -2,6 +2,7 @@ package com.time.collection.basic;
 
 import java.util.Iterator;
 import java.util.PriorityQueue;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.junit.Test;
 
@@ -24,8 +25,33 @@ public class QueueBasicTest {
 			String element = (String) it.next();
 			System.out.println(element);
 		}
-		
-		
 	}
 
+}
+
+class FIFOEntry<E extends Comparable<? super E>> implements Comparable<FIFOEntry<E>> {
+
+	static final AtomicLong seq = new AtomicLong(0);
+
+	final long seqNum;
+
+	final E entry;
+
+	public FIFOEntry(E entry) {
+		seqNum = seq.getAndIncrement();
+		this.entry = entry;
+	}
+
+	public E getEntry() {
+		return entry;
+	}
+
+	@Override
+	public int compareTo(FIFOEntry<E> other) {
+		// TODO Auto-generated method stub
+		int res = entry.compareTo(other.entry);
+		if (res == 0 && other.entry != this.entry)
+			res = (seqNum < other.seqNum ? -1 : 1);
+		return res;
+	}
 }
