fd0e39ed7f544c75bc8961331edd52b8fc2878cd
littleyang <littleyangqh@gmail.com>
2016-06-30 16:28:31 +0800

    add sum

---
+++ time-common/src/test/java/PowerXN.java
@@ -0,0 +1,17 @@
+
+public class PowerXN {
+	
+	public static void main(String[] args){
+		
+		
+		
+		
+	}
+	
+	public static long power(int x, int n){
+		
+		
+		return 0;
+	}
+
+}
---
+++ time-common/src/test/java/SumTwo.java
@@ -0,0 +1,37 @@
+
+public class SumTwo {
+	
+	public static void main(String[] args){
+		
+		int a = 3, b =1;
+		
+		int r1 = getSum(a, b);
+		
+		System.out.println(r1);
+		
+		int r2 = getSumTwo(a,b);
+		
+		
+		System.out.println(r2);
+		
+	}
+	
+	
+	public static int getSum(int a, int b){
+		
+		return a + b;
+	}
+
+	
+	public static int getSumTwo(int a, int b){
+		
+		while(b != 0){
+			int c = a^b;
+			b = (a&b)<<1;
+			a = c;
+		}
+		return a;
+	}
+
+
+}
