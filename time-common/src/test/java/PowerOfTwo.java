e545b31362072ea36210832774811f8d7ae1cce4
littleyang <littleyangqh@gmail.com>
2016-07-01 10:59:21 +0800

    add

--- time-common/src/test/java/PowerOfTwo.java
+++ time-common/src/test/java/PowerOfTwo.java
@@ -20,6 +20,25 @@ public class PowerOfTwo {
 		System.out.println("isPowerOfTwo(16) " + isPowerOfTwo(16));
 		
 		System.out.println("isPowerOfTwo(18) " + isPowerOfTwo(18));
+		
+		
+		System.out.println("isPowerOfTwoAnother(0) " + isPowerOfTwoAnother(0));
+		
+		System.out.println("isPowerOfTwoAnother(2) " + isPowerOfTwoAnother(2));
+		
+		System.out.println("isPowerOfTwoAnother(4) " + isPowerOfTwoAnother(4));
+		
+		System.out.println("isPowerOfTwoAnother(6) " + isPowerOfTwoAnother(6));
+		
+		System.out.println("isPowerOfTwoAnother(8) " + isPowerOfTwoAnother(8));
+		
+		System.out.println("isPowerOfTwoAnother(10) " + isPowerOfTwoAnother(10));
+		
+		System.out.println("isPowerOfTwoAnother(12) " + isPowerOfTwoAnother(14));
+		
+		System.out.println("isPowerOfTwoAnother(16) " + isPowerOfTwoAnother(16));
+		
+		System.out.println("isPowerOfTwoAnother(18) " + isPowerOfTwoAnother(18));
 	}
 	
 	public static boolean isPowerOfTwo(int n){
@@ -29,6 +48,15 @@ public class PowerOfTwo {
 		}
 		
 		return (n&(n-1))==0?true:false;
+	}
+	
+	public static boolean isPowerOfTwoAnother(int n){
+		
+		if(n==0||n<0){
+			return false;
+		}
+		
+		return (Math.log10(n)/Math.log10(2))%1==0?true:false;
 	}
 
 }
