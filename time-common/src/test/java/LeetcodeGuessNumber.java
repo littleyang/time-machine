a88efd48e4b16df24902f2b1743485e9a34e603f
littleyang <littleyangqh@gmail.com>
2016-07-25 19:33:25 +0800

    add hadoop

--- time-common/src/test/java/LeetcodeGuessNumber.java
+++ time-common/src/test/java/LeetcodeGuessNumber.java
@@ -3,8 +3,11 @@ public class LeetcodeGuessNumber {
 	
 	public static void main(String[] args){
 		
+		int n =10;
 		
+		int result = guess(n);
 		
+		System.out.println(result);
 		
 	}
 	
@@ -14,13 +17,19 @@ public class LeetcodeGuessNumber {
 		int L = 1,R = num;
         while(L <= R){
             int mid = L + ((R - L) >> 1);
-            int res = guess(mid);
+            int res = guessNumber(mid);
             if(res == 0) return mid;
             else if(res == 1)  L = mid + 1;
             else R = mid - 1;
         }
         return L;
 		
+	}
+
+
+	private static int guessNumber(int n) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
