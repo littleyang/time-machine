a6f2e6ce813734fb86abc9d7ccfc70594ec2f7e2
littleyang <littleyangqh@gmail.com>
2016-08-02 18:33:33 +0800

    add Intersection of two arrays

--- time-common/src/test/java/Intersection.java
+++ time-common/src/test/java/Intersection.java
@@ -11,27 +11,54 @@ public class Intersection {
         int[] a = {1,2,3,4};
         int[] b = {2,3};
 
-        Arrays.sort(a);
-        Arrays.sort(b);
+//        Arrays.sort(a);
+//        Arrays.sort(b);
+//
+//        ArrayList<Integer> result = new ArrayList<Integer>();
+//
+//        for(int i=0;i<a.length;i++){
+//
+//            if(i==0 || (i>0 && a[i]!=a[i-1])){
+//
+//                if(binarySearch(b,a[i])){
+//                    result.add(a[i]);
+//                }
+//
+////                if(Arrays.binarySearch(b, a[i])>-1){
+////                    result.add(a[i]);
+////                }
+//            }
+//        }
+//
+//        System.out.println(result.size());
+
+           System.out.print(intersection(a,b).length);
+    }
+
+    public static int[] intersection(int[] nums1, int[] nums2){
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
 
         ArrayList<Integer> result = new ArrayList<Integer>();
 
-        for(int i=0;i<a.length;i++){
+        for(int i=0;i<nums1.length;i++){
 
-            if(i==0 || (i>0 && a[i]!=a[i-1])){
+            if(i==0 || (i>0 && nums1[i]!=nums1[i-1])){
 
-                if(binarySearch(b,a[i])){
-                    result.add(a[i]);
+                if(binarySearch(nums2,nums1[i])){
+                    result.add(nums1[i]);
                 }
-
-//                if(Arrays.binarySearch(b, a[i])>-1){
-//                    result.add(a[i]);
-//                }
             }
         }
+        //return result.toArray();
 
-        System.out.println(result.size());
-
+        int[] res = new int[result.size()];
+        int k=0;
+        for(int i: result){
+            res[k++] = i;
+        }
+        return res;
     }
 
     public static boolean binarySearch(int[] arrays, int target){
