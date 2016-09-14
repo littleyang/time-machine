3d2c0b0c1ae25629b998a3879fa43791cb66d0d4
zhouyang(周阳) <young.zy@alibabapictures.com>
2016-09-14 14:10:37 +0800

    small

--- time-common/src/test/java/com/time/study/leetcode/PartionListNode.java
+++ time-common/src/test/java/com/time/study/leetcode/PartionListNode.java
@@ -36,7 +36,28 @@ public class PartionListNode {
 
     public static ListNode partition(ListNode head, int x) {
 
-        return null;
+        if(head == null){
+            return null;
+        }
+
+        ListNode leftDummy = new ListNode(0);
+        ListNode rightDummy = new ListNode(0);
+        ListNode left = leftDummy, right = rightDummy;
+
+        while (head != null) {
+            if (head.val < x) {
+                left.next = head;
+                left = head;
+            } else {
+                right.next = head;
+                right = head;
+            }
+            head = head.next;
+        }
+
+        right.next = null;
+        left.next = rightDummy.next;
+        return leftDummy.next;
     }
 }
 
