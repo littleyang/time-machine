e5b3f332041e5b02cd45758bf34bd53626e98c8d
zhouyang(周阳) <young.zy@alibabapictures.com>
2016-09-14 11:53:24 +0800

    add list node partion

---
+++ time-common/src/test/java/com/time/study/leetcode/PartionListNode.java
@@ -0,0 +1,23 @@
+package com.time.study.leetcode;
+
+/**
+ * @description: 分割链表使得小于某个值X的所有链表在左边
+ * @author: yang.zhou
+ * @version: 1.0.0
+ * @since: 2016-09-14 11:51 AM
+ */
+
+public class PartionListNode {
+}
+
+
+class ListNode{
+
+    int val;
+
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
