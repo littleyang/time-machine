945e669a4ffb5c2a10200ee5b6a4c3d1a04017b2
zhouyang(周阳) <young.zy@alibabapictures.com>
2016-09-14 12:01:23 +0800

    create node

--- time-common/src/test/java/com/time/study/leetcode/PartionListNode.java
+++ time-common/src/test/java/com/time/study/leetcode/PartionListNode.java
@@ -8,6 +8,36 @@ package com.time.study.leetcode;
  */
 
 public class PartionListNode {
+
+    public static void main(String[] args){
+
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(4);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(2);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(2);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+
+
+        int x = 3;
+
+        ListNode result = partition(node1,x);
+
+        System.out.println(result.val);
+
+
+    }
+
+    public static ListNode partition(ListNode head, int x) {
+
+        return null;
+    }
 }
 
 
